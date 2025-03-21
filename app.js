9e2b03e45d78ab73406d65c34e4dcd847cdf6b6c
pablodavid15 <pablodavidp.santana@gmail.com>
2025-03-21 20:58:22 -0300

    Projeto node

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 //sequelize
     const Sequelize = require('sequelize')
     require('./models/Produto')
+    require('./models/Servico')
 
     app.get('/', (req, res) => {
         res.send('Olá, Cris')
@@ -29,13 +30,61 @@
                     })
                     console.log(resultadoCreate);
 
+                //Criando um array das informações
                     const produtos = await Produto.findAll();
                     console.log(produtos);
-
+                
+                //procurando pelo id
                     const produto = await Produto.findByPk(1);
                     console.log(produto);
 
                     console.log(1)
+
+                //update
+                    produto.nome = 'Mouse gamer';
+
+                    const resultadoSave = await produto.save();
+                    console.log(resultadoSave)
+
+            }catch (err) {
+                console.log(err);
+            }
+        })();
+
+    //Tabela de serviços
+        (async () => {
+            const database = require('./conexao/ds')
+            const Servico = require('./models/Servico')
+
+            try {
+                //Crienado a  tabela
+                    const resultado = await database.sync();
+                    console.log('ok')
+                    console.log(`tudo certo: ${resultado}`);
+
+                    const resultadoCreate = await Servico.create({
+                        nome: 'Limpeza de PCs',
+                        preco: 50,
+                        descricao: 'Serviços para limpezas de PCs'
+                    })
+                    console.log(resultadoCreate);
+
+                //Criando um array das informações
+                    const servicos = await Servico.findAll();
+                    console.log(servicos);
+
+                //procurando pelo id
+                    const servico = await Servico.findByPk(1);
+                    console.log(servico);
+
+                    console.log(1)
+
+                //update
+                servico.nome = 'Limpeza de PCs ultra';
+
+                const resultadoSave = await servico.save();
+                console.log(resultadoSave)
+
             }catch (err) {
                 console.log(err);
             }
---
+++ models/Servico.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize');
+const database = require('../conexao/ds');
+
+const Servico = database.define('servico', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }, 
+    nome:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    preco: {
+        type: Sequelize.DOUBLE
+    },
+
+    descricao: Sequelize.STRING
+
+})
+
+module.exports = Servico;
